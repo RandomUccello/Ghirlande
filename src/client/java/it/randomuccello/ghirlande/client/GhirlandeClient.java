58da8d51e79ae5a1285aad0bf37dd97747835c3b
RandomUccello <stefanolepera28@gmail.com>
2026-09-19 07:10:03 +0200

    Add client mod entrypoint

---
+++ src/client/java/it/randomuccello/ghirlande/client/GhirlandeClient.java
@@ -0,0 +1,10 @@
+package it.randomuccello.ghirlande.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class GhirlandeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Client-side rendering registrations will be added after the common logic compiles.
+    }
+}
